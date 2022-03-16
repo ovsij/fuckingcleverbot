c08f196d499892f044d5e0a3d1433de782c5d505
ovsij <v.ovsij@yandex.ru>
2022-03-16 17:29:26 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+TOKEN = os.environ['TOKEN']
+TOKEN_TELEGRAPH = os.environ['TOKEN_TELEGRAPH']
+
+
+
+database = os.environ['database']
+user = os.environ['user']
+password = os.environ['password']
+host = os.environ['host']
+port = 5432
+
+
